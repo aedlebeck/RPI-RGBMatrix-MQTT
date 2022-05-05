94785785b16648c2c870ad17848fbb94ad423000
aedlebeck <aedlebeck@gmail.com>
2022-05-05 14:07:44 -0700

    Update example.py
    Updated

--- example.py
+++ example.py
@@ -1,30 +1,46 @@
+import signal
 import os
 import time
 from ws281xMatrix import WS281xMatrix
 
 path = os.path.dirname(__file__)
 im = os.path.join(path, 'curImg.png')
-ani = os.path.join(path, 'curGif.gif')
+gif = os.path.join(path, 'curGif.gif')
 
 screen = WS281xMatrix()
 
-def change_color(color):
-    screen.next_frame(screen.blank_frame(color))
-
-try:
-    while True:
-        print('Red')
-        change_color((255,0,0))
-        time.sleep(2)
-        print('Green')
-        change_color((0,255,0))
-        time.sleep(2)
-        print('Blue')
-        change_color((0,0,255))
-        time.sleep(2)
-        screen.render_image(im)
-        time.sleep(10)
-        screen.render_animation(ani)
-        time.sleep(20)
-except KeyboardInterrupt:
+#Handles signal iterrupts
+def sigint_handler(signal, frame):
+    print("KeyboardInterrupt\m")
     screen.kill()
+    sys.exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
+
+
+def clearScreen():
+    screen.reset()
+ 
+def setImg(s):
+    clearScreen()
+    screen.render_image(s)
+
+
+def setGif(s):
+    clearScreen()
+    screen.render_animation(s, loops = 100)
+
+
+#Tuple R,G,B for color
+def setColor(color):
+    clearScreen()
+    screen.next_frame(screen.blank_frame(color))
+  
+#Test matrix
+print('Blue')
+setColor((0,0,255))
+time.sleep(3)
+screen.render_image(im)
+time.sleep(10)
+screen.render_animation(ani)
+time.sleep(20)
+
